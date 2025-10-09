6b5eb1b06926c6c5bc86785de3ccad473ccb6fe6
onjunge24553 <kelvinnjunge98@gmail.com>
2025-10-09 14:48:58 +0300

    Create Height,bankbalance,age.c

---
+++ Height,bankbalance,age.c
@@ -0,0 +1,23 @@
+*/
+muigai kelvin njunge,
+CT101/G/24553/24,
+3 OCTOBER 2025.
+/*
+#include<stdio.h>
+int main()
+{
+int age;
+float bankbalance;
+int height;
+printf("enter your age:\n");
+scanf("%d",&age);
+printf("you entered:%d\n",age);
+printf("enter your bankbalance:\n");
+scanf("%f",&bankbalance);
+printf("you entered:%.2f\n",bankbalance);
+printf("enter height:\n");
+scanf("%d",&height);
+printf("you entered:%d",height);
+return 0;
+}
+
