4ca7ee1221f61ff1618728c61a04c347ba3f99dd
onjunge24553 <kelvinnjunge98@gmail.com>
2025-10-09 14:44:23 +0300

    Create Units.c

---
+++ Units.c
@@ -0,0 +1,21 @@
+*/
+  muigai kelvin njunge,
+  CT101/G/24553/24,
+  3 October 2025
+/*
+  
+#include<stdio.h>
+int main()
+{
+char units[7][60]= {
+"PHYSICS ",
+"FUNDAMENTALS OF ICT ",
+"COMMUNICATION SKILLS ",
+"MATHMETICS SCIENCE ",
+"ANALOGUE ELECTRONICS ",
+"DISCRETE MATHS ",
+"INTRODUCTION TO PROGRAMMING "};
+for (int i=0;i<7;i++)
+printf("units:%s\n",units[i]);
+return 0;
+
