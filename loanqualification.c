6a77e1d50d52d776ec01fa94388cf7568ff60824
onjunge24553 <kelvinnjunge98@gmail.com>
2025-10-11 09:48:38 +0300

    Create loanqualification.c

---
+++ loanqualification.c
@@ -0,0 +1,22 @@
+*/
+muigai kelvin njunge,
+CT101/G/24553/24,
+11 OCTOBER 2025
+/*
+#include<stdio.h>
+int main(){
+int age;
+float salary;
+printf("enter your age:\n");
+scanf("%d",&age);
+printf("you entered:%d\n",age);
+printf("enter your salary:\n");
+scanf("%f",&salary);
+printf("you entered:%.2f",salary);
+if(age>=18 &&salary>=21000)
+printf("congratulations you qualify for a loan:\n");
+else
+printf("unfortunately you don't qualify for a loan:\n");
+return 0;
+}
+
