23c3e0895f41d94a1cc32e3502064caf643c8d64
onjunge24553 <kelvinnjunge98@gmail.com>
2025-10-12 06:47:14 +0300

    Create databalance.c

---
+++ databalance.c
@@ -0,0 +1,33 @@
+*/
+muigai kelvin njunge 
+CT101/G/24553/24
+11october2025
+/*
+#include<stdio.h>
+int main(){
+int option;
+printf("1\t100mb\t\t50\n");
+printf("2\t500mb\t\t200\n");
+printf("3\t1GB\t\t350\n");
+printf("4\t2GB\t\t600\n");
+printf("option\tbundles\t\tcost");
+scanf("%d",&option);
+switch(option){
+case 1:
+printf("you selected100mb.cost50\n");
+break;
+case 2:
+printf("you selected500mb.cost200\n");
+break;
+case 3:
+printf("you selected1Gb.cost350\n");
+break;
+case 4:
+printf("you selected2GB.cost600\n");
+break;
+default:
+printf("invalid option!please enter option 1_4\n");
+}
+return 0;
+}
+
