35e4502d251ed068bd9a7fc56793dac1d62b3ae7
onjunge24553 <kelvinnjunge98@gmail.com>
2025-10-13 11:14:36 +0300

    password.c

---
+++ password.c
@@ -0,0 +1,17 @@
+/*
+Muigai kelvin njunge
+CT101/G/24553/24
+12 OCTOBER 2025
+*/
+#include<stdio.h>
+int main(){
+int password;
+do{
+printf("enter password:\n");
+scanf("%d",&password);
+}
+while(password !=1234);
+printf("access granted\n");
+return 0;
+}
+
